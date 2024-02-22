93a8225f7427b5184a445ffe363b5b68394b9feb
Mohamed Amine <ma.abahmane@gmail.com>
2024-02-23 00:36:04 +0100

    Update V : Game Controls & Music/Sounds 🔥

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-// Select all canvas elements from html
+// Select canvas elements from html
 const canvas = document.querySelector('canvas')
 
-// Returns an object that provides methods and properties for drawing
-// and manipulating images and graphics on a canvas element
+/* Returns an object that provides methods and properties for drawing
+ *   and manipulating images and graphics on a canvas element
+ */
 const c = canvas.getContext('2d')
-
 // Set the width & height to fill the screen
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+
+//| QUERY Selection |\\
 const score = document.querySelector('#score')
 const scoreBrd = document.querySelector('#scoreBrd')
 const startGameBtm = document.querySelector('#startGameBtm')
 const startGameBrd = document.querySelector('#startGameBrd')
 
+const musicControl = document.querySelector('#musicControl')
+const soundControl = document.querySelector('#soundControl')
+const gameControl = document.querySelector('#gameControl')
+
+
+//|\\ CLASS declarations //|\\
 /// Player class [User Base Character]
 class Player {
     constructor(x, y, color, radius){
@@ -125,6 +133,60 @@ class Particle {
 }
 
 
+//\ AUDIO Settings /\\
+
+// Sound player \\
+function sounder(path, vol=1)
+{
+    if (soundOn)
+    {
+        const player = new Audio
+        player.src = path
+        player.volume = vol
+        player.play()
+    }
+}
+
+/// AUDIO player \\\
+let music
+let soundOn = true
+let musicOn = true
+function musiker()
+{
+    // Play random background music
+    music = new Audio
+    rm = Math.floor(Math.random() * 8.99)
+    music.src = `music/m${rm}.mp3`
+    music.loop = true
+    music.volume = 0.9
+    music.play()
+
+    // Play or Pause music on music button click
+    musicControl.addEventListener('click', () => {
+        if (musicOn)
+        {
+            music.pause()
+            musicControl.style.backgroundImage = 'url(ico/mmb.png)'
+        } else {
+            music.play()
+            musicControl.style.backgroundImage = 'url(ico/mm.png)'
+        }
+        musicOn = !musicOn
+    })
+
+    // Play or Pause music on music button click
+    soundControl.addEventListener('click', () => {
+        if (soundOn)
+        {
+            soundControl.style.backgroundImage = 'url(ico/nnn.png)'
+        } else {
+            soundControl.style.backgroundImage = 'url(ico/nn.png)'
+        }
+        soundOn = !soundOn
+    })
+}
+
+
 /// player creation
 let player = new Player(innerWidth / 2, innerHeight / 2, 'royalblue', 50)
 
@@ -139,68 +201,71 @@ let PARTICLES = []
 //'#b9cefb', '#660000', '#e7c459', '#371f30', '#fff49f',
 //'#d89eff', '#fff49f', '#2d2c4e', '#ff8c8c', '#dc143c']
 
-// RESTART GAME
+/// RESTART GAME
 function init()
-{   
-    // Select all canvas elements from html
-    const canvas = document.querySelector('canvas')
+{
 
-    // Returns an object that provides methods and properties for drawing
-    // and manipulating images and graphics on a canvas element
-    const c = canvas.getContext('2d')
+    c.clearRect(0, 0, canvas.width, canvas.height)
 
-    // Set the width & height to fill the screen
-    canvas.width = innerWidth
-    canvas.height = innerHeight
     /// player creation
     player = new Player(innerWidth / 2, innerHeight / 2, 'royalblue', 50)
+
     /// Projectiles list
     DARTS = []
     /// Enemies list
     TARGETS = []
     /// Dart and Target collision affect
     PARTICLES = []
+
+    //\\ This line was added due to Canvas having replay performance issues
+     if (SCORE > 0) location.reload()
+
     // reset score
     SCORE = 0
     score.innerHTML = SCORE
     scoreBrd.innerHTML = SCORE
 }
 
-// Spawn Targets
+/// SPAWN Targets
 function spawnTargets() {
     // make a call each 1500 millisecond
     setInterval(() => {
-        //# Random Target size
-        const radius = Math.random() * (50 - 15) + 15
-        //# Random Target Spawn #\\
-        if (Math.random() < 0.5) {
-            // Out of screen Width, random height
-            var x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
-            var y = Math.random() * canvas.height
-        } else {
-            // Out of screen Height, random width
-            var x = Math.random() * canvas.width
-            var y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        if (!PAUSED)
+        {
+            //# Random Target size
+            const radius = Math.random() * (50 - 15) + 15
+            //# Random Target Spawn #\\
+            if (Math.random() < 0.5) {
+                // Out of screen Width, random height
+                var x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+                var y = Math.random() * canvas.height
+            } else {
+                // Out of screen Height, random width
+                var x = Math.random() * canvas.width
+                var y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+            }
+            /// Target creation
+            // random Target color pick
+            const color = `hsl(${Math.random() * 360}, 50%, 50%)`
+            const angle = Math.atan2(canvas.height / 2 - y , canvas.width / 2 - x)
+            const velocity = {
+                x: Math.cos(angle),
+                y: Math.sin(angle)
+            }
+            TARGETS.push(new Targets(x, y, color, radius, velocity))
         }
-        /// Target creation
-        // random Target color pick
-        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
-        const angle = Math.atan2(canvas.height / 2 - y , canvas.width / 2 - x)
-        const velocity = {
-            x: Math.cos(angle),
-            y: Math.sin(angle)
-        }
-        TARGETS.push(new Targets(x, y, color, radius, velocity))
     }, 1400)
 }
 
 
+
+//-/ Animation GAME LOOP \-\\
 let animeID
 let SCORE = 0
-
-/// Animation GAME LOOP \\\
 function animator() {
-    animeID = requestAnimationFrame(animator)
+    if (!PAUSED)
+        animeID = requestAnimationFrame(animator)
+
     // Clear Frame
     c.fillStyle = 'rgba(0, 0 , 0 , .1)'
     c.fillRect(0, 0, canvas.width, canvas.height)
@@ -237,6 +302,8 @@ function animator() {
         const dist = Math.hypot(player.x - target.x, player.y - target.y)
         if (dist - target.radius - player.radius < 1)
         {
+            music.pause()
+            sounder('music/game.mp3')
             cancelAnimationFrame(animeID)
             startGameBrd.style.display = 'flex'
             scoreBrd.innerHTML = SCORE
@@ -273,6 +340,10 @@ function animator() {
                     setTimeout(() => {
                         DARTS.splice(Dndx, 1)
                     }, 0)
+
+                    // Enemy damage sound affect
+                    sounder('music/hit.mp3', 0.8)
+
                 } else {
 
                     SCORE += 250
@@ -282,7 +353,12 @@ function animator() {
                         TARGETS.splice(Tndx, 1)
                         DARTS.splice(Dndx, 1)
                     }, 0)
+
+                    // Enemy explode sound affect
+                    sounder('music/pop.mp3', 0.9)
                 }
+
+
             }
         })
     })
@@ -300,8 +376,10 @@ function Normal() {
     // event contains mouse click information
     addEventListener('click', (event) => {
         // Get the angle of the projectile
-        const angle = Math.atan2(event.clientY - canvas.height / 2 , event.clientX - canvas.width / 2)
-        const velocity = {
+        if (!PAUSED)
+        {
+            const angle = Math.atan2(event.clientY - canvas.height / 2 , event.clientX - canvas.width / 2)
+            const velocity = {
             x: Math.cos(angle),
             y: Math.sin(angle)
         }
@@ -312,11 +390,14 @@ function Normal() {
             'white',
             5,
             velocity
-        ))
+            ))
+
+            sounder('music/shot.mp3', 0.6)
+        }
     })
 }
 
-/// OVERDRIVE MODE: WIn this mode, Projectiles are shot as long as the mouse click is down
+/// OVERDRIVE MODE: In this mode, Projectiles are shot as long as the mouse click is down
 // listen for mouse down
 function Overdrive()
 {
@@ -324,11 +405,11 @@ function Overdrive()
 
     // Function to create a projectile
     function createProjectile(event) {
-        const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
+        const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
         const velocity = {
             x: Math.cos(angle),
             y: Math.sin(angle)
-        };
+        }
 
         // Projectile creation
         DARTS.push(new Dart(
@@ -337,28 +418,45 @@ function Overdrive()
             'red',
             5,
             velocity
-        ));
+        ))
     }
 
-    //const createProjectileThrottled = _.throttle(createProjectile, 100);
+    //const createProjectileThrottled = _.throttle(createProjectile, 100)
 
     // Listen for mouse down event
     addEventListener('mousedown', (event) => {
-        isMouseDown = true;
-        createProjectile(event);
-    });
-    
+        isMouseDown = true
+        createProjectile(event)
+    })
+
     // Listen for mouse move event
     addEventListener('mousemove', (event) => {
-        createProjectile(event);
-    });
+        createProjectile(event)
+    })
     // Listen for mouse up event
     addEventListener('mouseup', () => {
-        isMouseDown = false;
-    });
+        isMouseDown = false
+    })
 }
 
 
+// Game PAUSING ||<| \\
+let PAUSED = false
+function pausePlay() {
+    // Toggle the PAUSED flag when the sound control button is clicked
+    gameControl.addEventListener('click', () => {
+        PAUSED = !PAUSED
+        if (PAUSED) {
+            // If paused, cancel the animation frame
+            cancelAnimationFrame(animeID)
+            gameControl.style.backgroundImage = 'url(ico/playn.png)'
+        } else {
+            // If unpaused, resume animation
+            animeID = requestAnimationFrame(animator)
+            gameControl.style.backgroundImage = 'url(ico/pausen.png)'
+        }
+    })
+}
 
 /// START GAME \\\
 startGameBtm.addEventListener('click', () => {
@@ -367,7 +465,15 @@ startGameBtm.addEventListener('click', () => {
     animator()
     spawnTargets()
 
-    // Drive type
-    Normal()
-    //Overdrive()
+    sounder('music/play.mp3')
+
+    // play background music
+    setTimeout(() => {
+        // Background Music
+        musiker()
+        pausePlay()
+        // Drive type
+        Normal()
+        //Overdrive()
+    }, 700)
 })
