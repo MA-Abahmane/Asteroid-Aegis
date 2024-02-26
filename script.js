580fdcba772a2ca4dea2f629fcdda684be62481c
Mohamed Amine <ma.abahmane@gmail.com>
2024-02-26 04:12:52 +0100

    Update XIII : Username UI & Game Power UPs 🔮

--- script.js
+++ script.js
@@ -13,18 +13,34 @@ canvas.height = innerHeight
 //| QUERY Selection |\\
 const score = document.querySelector('#score')
 const scoreBrd = document.querySelector('#scoreBrd')
-const startGameBtm = document.querySelector('#startGameBtm')
+const startGameBtm = document.querySelector('#startGameBtn')
 const startGameBrd = document.querySelector('#startGameBrd')
 
 const musicControl = document.querySelector('#musicControl')
 const soundControl = document.querySelector('#soundControl')
 const gameControl = document.querySelector('#gameControl')
 
+const layer1 = document.querySelector('.layer1')
+const layer2 = document.querySelector('.layer2')
+const layer3 = document.querySelector('.layer3')
+const outer = document.querySelector('.outer')
+const inner = document.querySelector('.inner')
+const percentage = document.querySelector('span')
 
-let contain2 = document.querySelector('.contain2')
-let outer = document.querySelector('.outer')
-let inner = document.querySelector('.inner')
-let percentage = document.querySelector('span')
+const processing = document.querySelector('.processing')
+const p1 = document.querySelector('.p1')
+const p2 = document.querySelector('.p2')
+const p3 = document.querySelector('.p3')
+
+const layer4 = document.querySelector('.layer4')
+const layer5 = document.querySelector('.layer5')
+const layer6 = document.querySelector('.layer6')
+const save_btn = document.querySelector('#save-btn')
+const alias = document.querySelector('.alias')
+
+const power1 = document.querySelector('#power1')
+const power2 = document.querySelector('#power2')
+const power3 = document.querySelector('#power3')
 
 
 //|\\ CLASS declarations //|\\
@@ -180,29 +196,27 @@ function sounder(path, vol=1)
 let music
 let soundOn = true
 let musicOn = true
-let _music
-let _sound
 function musiker()
 {
     // Play random background music
     music = new Audio
     rm = Math.floor(Math.random() * 8.99)
-    music.src = `music/m${rm}.mp3`
+    music.src = `TuneBox/m${rm}.mp3`
     music.loop = true
     music.volume = 0.9
     music.play()
     musicOn = true
-    musicControl.style.backgroundImage = 'url(ico/mm.png)'
+    musicControl.style.backgroundImage = 'url(VisualVault/mm.png)'
 
     // Play or Pause music on music button click
     musicControl.addEventListener('click', _music = () => {
         if (musicOn)
         {
             music.pause()
-            musicControl.style.backgroundImage = 'url(ico/mmb.png)'
+            musicControl.style.backgroundImage = 'var(--music-off-logo)'
         } else {
             music.play()
-            musicControl.style.backgroundImage = 'url(ico/mm.png)'
+            musicControl.style.backgroundImage = 'url(VisualVault/mm.png)'
         }
         musicOn = !musicOn
     })
@@ -211,9 +225,9 @@ function musiker()
     soundControl.addEventListener('click', _sound = () => {
         if (soundOn)
         {
-            soundControl.style.backgroundImage = 'url(ico/nnn.png)'
+            soundControl.style.backgroundImage = 'var(--sound-off-logo)'
         } else {
-            soundControl.style.backgroundImage = 'url(ico/nn.png)'
+            soundControl.style.backgroundImage = 'url(VisualVault/nn.png)'
         }
         soundOn = !soundOn
     })
@@ -222,21 +236,40 @@ function musiker()
 
 /// player var
 let player
+/// player Username
+let USERNAME = 'Player'
 /// Is the Game On?
 let GAME0N = true
+/// player score
+let SCORE = 0
 /// Projectiles list
 let DARTS = []
 /// Enemies list
 let TARGETS = []
 /// Particles list
 let PARTICLES = []
+/// Power UPs Allow
+let alwPower1 = true
+let alwPower2 = false
+let alwPower3 = false
 /// intervals list
-let intervalIDs = [];
+let intervalIDs = []
+/// Event Listener function vars
+let _music
+let _sound
+let _pauser
+let _shooter
+let _mousedown
+let _mousemove
+let _mouseup
 
 
 /// RESTART GAME
 function init()
-{
+{   
+    // clear and set canvas size
+    canvas.width = innerWidth
+    canvas.height = innerHeight
     c.clearRect(0, 0, canvas.width, canvas.height)
 
     /// player creation
@@ -249,6 +282,13 @@ function init()
     TARGETS = []
     /// Dart and Target collision affect
     PARTICLES = []
+    /// Power UPs Allow
+    alwPower1 = true
+    alwPower2 = false
+    alwPower3 = false
+    power2.style.opacity = 1
+    power2.style.opacity = 0.5
+    power3.style.opacity = 0.5
 
     //\\ This line was added due to Canvas having replay performance issues
     //if (!GAME0N) location.reload()
@@ -266,9 +306,11 @@ function init()
 
 /// SPAWN Targets
 let timer = 0
+let spawnTargetsID
 function spawnTargets() {
     // SetInterval; make a call each 1500 millisecond
-    let id = setInterval(() => {
+    let activated2, activated3 = false
+    spawnTargetsID = setInterval(() => {
         if (!PAUSED && GAME0N)
         {
             //# Random Target size
@@ -293,7 +335,6 @@ function spawnTargets() {
             }
             TARGETS.push(new Targets(x, y, color, radius, velocity))
 
-
             //~ Unleash The COLOSSAL Target ~\\
             if (SCORE > 50000)
             {
@@ -312,38 +353,49 @@ function spawnTargets() {
                 }
                 else
                     timer++
+            } else if (SCORE >= 2000 && !activated2) {
+                // Power UP 1 & 2 release
+                alwPower2 = true
+                activated2 = true
+                gsap.to(power2, { opacity: 1, duration: .5 });
+                console.log('on1');
+            } else if (SCORE >= 4000 && !activated3) {
+                alwPower3 = true
+                activated3 = true
+                gsap.to(power3, { opacity: 1, duration: .5 });
+                console.log('on2');
             }
-
         }
     }, 1200)
-    // save interval id
-    intervalIDs.push(id)
+    // save interval id for later clear
+    intervalIDs.push(spawnTargetsID)
 }
 
 /// Chameleon MODE; Make player change colors like a chameleon
 function chameleon() {
     let n = 0
     // go trough the color wheel
-    let id = setInterval(() => {
+    const id = setInterval(() => {
         if (!PAUSED && GAME0N)
         {
             // Green Sock Animation
             gsap.to(player, {
                 color: `hsl(${n}, 58%, 55%)`
             })
-            gsap.to(player, {
-                sfrRadius: 100
-            })
             n += 2
         }
     }, 1000)
+    // save interval id
     intervalIDs.push(id)
+
+    // set up player sphere
+    gsap.to(player, { sfrRadius: 100 })
+
 }
 
 
 //-/ Animation GAME LOOP \-\\
 let animeID
-let SCORE = 0
 function animator() {
     if (!PAUSED)
         animeID = requestAnimationFrame(animator)
@@ -393,14 +445,19 @@ function animator() {
         {
             GAME0N = false
             music.pause()
-            sounder('music/game.mp3')
+            sounder('TuneBox/game.mp3')
 
+            // Pause The Animation loop
             cancelAnimationFrame(animeID)
+            // Kill all working Event Listeners
             removeEventListener('click', _shooter)
+            removeEventListener('mousedown', _mousedown)
+            removeEventListener('mousemove', _mousemove)
+            removeEventListener('mouseup', _mouseup)
             musicControl.removeEventListener('click', _music)
             soundControl.removeEventListener('click', _sound)
             gameControl.removeEventListener('click', _pauser)
-
+            // display the Game Board
             startGameBrd.style.display = 'flex'
             scoreBrd.innerHTML = SCORE
 
@@ -439,7 +496,7 @@ function animator() {
                     }, 0)
 
                     // Enemy damage sound affect
-                    sounder('music/hit.mp3', 0.9 )
+                    sounder('TuneBox/hit.mp3', 0.9 )
 
                 } else {
 
@@ -447,7 +504,7 @@ function animator() {
                     score.innerHTML = SCORE
 
                     // Enemy destruction sound affect
-                    TARGETS[Tndx].colossal ? sounder('music/bom.mp3') : sounder('music/pop.mp3')
+                    TARGETS[Tndx].colossal ? sounder('TuneBox/bom.mp3') : sounder('TuneBox/pop.mp3')
 
                     // setTimeout waits until the next frame to perform changes
                     // Enemy explode sound affect
@@ -455,7 +512,6 @@ function animator() {
                         TARGETS.splice(Tndx, 1)
                         DARTS.splice(Dndx, 1)
                     }, 0)
-
 
                 }
 
@@ -465,13 +521,11 @@ function animator() {
 }
 
 
-
-
 /// InGame Click Management \\\
 
 /// NORMAL MODE: In this mode, Projectiles are shot for each mouse click
 // listen for mouse clicks
-let _shooter
+let dSize = 5
 function Normal() {
     // listen for mouse clicks
     // event contains mouse click information
@@ -489,11 +543,11 @@ function Normal() {
                 canvas.width / 2,
                 canvas.height / 2,
                 'white',
-                5,
+                dSize,
                 velocity
             ))
 
-            sounder('music/shot.mp3', 0.6)
+            sounder('TuneBox/shot.mp3', 0.6)
         }
     })
 }
@@ -515,23 +569,23 @@ function Overdrive()
         DARTS.push(new Dart(
             canvas.width / 2,
             canvas.height / 2,
-            'red',
+            player.color,
             5,
             velocity
         ))
     }
     //const createProjectileThrottled = _.throttle(createProjectile, 100)
     // Listen for mouse down event
-    addEventListener('mousedown', (event) => {
+    addEventListener('mousedown', _mousedown = (event) => {
         isMouseDown = true
         createProjectile(event)
     })
     // Listen for mouse move event
-    addEventListener('mousemove', (event) => {
+    addEventListener('mousemove', _mousemove = (event) => {
         createProjectile(event)
     })
     // Listen for mouse up event
-    addEventListener('mouseup', () => {
+    addEventListener('mouseup', _mouseup = () => {
         isMouseDown = false
     })
 }
@@ -539,7 +593,6 @@ function Overdrive()
 
 // Game PAUSING ||<| \\
 let PAUSED = false
-let _pauser
 function pausePlay() {
     // Toggle the PAUSED flag when the sound control button is clicked
     gameControl.addEventListener('click', _pauser = () => {
@@ -549,38 +602,163 @@ function pausePlay() {
             if (PAUSED) {
                 // If paused, cancel the animation frame
                 cancelAnimationFrame(animeID)
-                gameControl.style.backgroundImage = 'url(ico/playn.png)'
+                gameControl.style.backgroundImage = 'url(VisualVault/playn.png)'
             } else {
                 // If unpaused, resume animation
                 animeID = requestAnimationFrame(animator)
-                gameControl.style.backgroundImage = 'url(ico/pausen.png)'
+                gameControl.style.backgroundImage = 'url(VisualVault/pausen.png)'
             }
         }
     })
 }
 
-/// LOAD GAME \\\
+
+///\ LOAD GAME /\\\
 let count = 0;
 inner.addEventListener('click', loader = () => {
-    let id = setInterval(() => {
-        if (GAME0N)
-        {
-            if (count == 100) {
-                outer.classList.remove('active-loader')
-                outer.classList.add('active-loader2')
-                clearInterval()
-                contain2.style.opacity = 0
-                contain2.style.display = 'none'
-                contain2.removeEventListener('click', loader)
-            } else {
-                count++
-                percentage.textContent = count + '%'
-                outer.classList.add('active-loader')
+    // remove Event Listener (one click is needed)
+    inner.removeEventListener('click', loader)
+
+    /* Process board smooth appearance */
+    percentage.textContent = '0%'
+    setTimeout(() => {
+        gsap.to(p1, { opacity: 0.5, duration: 1 });
+        gsap.to(p2, { opacity: 0.5, duration: 2 });
+        gsap.to(p3, { opacity: 0.5, duration: 3 });
+    }, 1000)
+
+    setTimeout(() => {
+        /* Percent Loader */
+        const id = setInterval(() => {
+            if (GAME0N) {
+                if (count == 99) {
+                    // load to 100% and animate load screen out
+                    setTimeout(() => {
+                        percentage.textContent = '100%'
+                        outer.classList.remove('active-loader')
+                        outer.classList.add('active-loader2')
+                        layer3.style.left = '-100%'
+                        layer2.style.left = '-100%'
+                        layer1.style.left = '-100%'
+                    }, 1000)
+
+                    // clear load screen and Interval
+                    setTimeout(() => {
+                        layer3.style.display = 'none'
+                        layer2.style.display = 'none'
+                        layer1.style.display = 'none'
+                        clearInterval(id)
+                    }, 3000)
+
+                }
+                else {
+                    count++
+                    percentage.textContent = count + '%'
+                    outer.classList.add('active-loader')
+                }
             }
-        }
-    }, 80)
-    intervalIDs.push(id)
+        }, 5) // < Loading speed
+
+         /* Process appear one by one */
+        const ls = [p1, p2, p3];
+        let i = 1;
+        gsap.to(p1, { opacity: 1, scale: '1.5', duration: 0.5 });
+        const _id = setInterval(() => {
+            if (i < ls.length) {
+                gsap.to(ls[i], { opacity: 1, scale: '1.5', duration: 0.5 });
+                gsap.to(ls[i - 1], { opacity: 0.5, scale: '1', duration: 0.5 });
+                i++;
+            } else {
+                clearInterval(_id);
+            }
+        }, 2500);
+    }, 2000)
 })
+
+
+function setAlias() {
+    let input = document.querySelector('#text-box').value;
+    if (input.length > 0) {
+        USERNAME = input
+        pass()
+    }
+}
+
+function pass() {
+    alias.innerHTML = USERNAME
+    layer6.style.left = '100%'
+    layer5.style.left = '100%'
+    layer4.style.left = '100%'
+
+    setTimeout(() => {
+        layer6.style.display = 'none'
+        layer5.style.display = 'none'
+        layer4.style.display = 'none'
+    }, 3000)
+}
+
+
+function power_I() {
+    if (alwPower1) 
+    {   
+        clearInterval(spawnTargetsID)
+        gsap.to(player, {sfrRadius: 800, duration: 3, ease: "power1.inOut"});
+        setTimeout(() => {
+            gsap.to(player, {sfrRadius: 100, duration: 3, ease: "power1.inOut"});
+        }, 20000)
+
+        gsap.to(power1, { opacity: 0.5, duration: 0.5 });
+        alwPower1 = false
+    }
+
+    setTimeout(() => {
+        gsap.to(power1, { opacity: 1, duration: 0.5 });
+        alwPower1 = true
+    }, 50000)
+} 
+
+function power_II() {
+    if (alwPower2) 
+    {
+        removeEventListener('click', _shooter)
+        Overdrive()
+
+        setTimeout(() => {
+            removeEventListener('mousedown', _mousedown)
+            removeEventListener('mousemove', _mousemove)
+            removeEventListener('mouseup', _mouseup)
+            Normal()
+        }, 20000)
+
+        gsap.to(power2, { opacity: 0.5, duration: 0.5 });
+        alwPower2 = false
+    }
+
+    setTimeout(() => {
+        gsap.to(power2, { opacity: 1, duration: 0.5 });
+        alwPower2 = true
+    }, 80000)
+} 
+
+function power_III() {
+    if (alwPower3) 
+    {
+        gsap.to(player, {sfrRadius: 1200, duration: 1, ease: "power1.inOut"});
+        TARGETS = [];
+        setTimeout(() => {
+            gsap.to(player, {sfrRadius: 100, duration: 2, ease: "power1.inOut"});
+        }, 2500);
+        
+        gsap.to(power3, { opacity: 0.5, duration: 0.5 });
+        alwPower3 = false
+    }
+
+    setTimeout(() => {
+        gsap.to(power3, { opacity: 1, duration: 0.5 });
+        alwPower3 = true
+    }, 50000)
+
+}
 
 
 /// START GAME \\\
@@ -591,8 +769,9 @@ startGameBtm.addEventListener('click', () => {
     chameleon()
     spawnTargets()
 
-    sounder('music/play.mp3')
-    
+    sounder('TuneBox/play.mp3')
+
+
     // play background music
     setTimeout(() => {
         // Background Music
